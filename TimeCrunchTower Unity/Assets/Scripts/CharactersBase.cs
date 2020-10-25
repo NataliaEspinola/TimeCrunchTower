784ca305948f064cd07c2ff796b1b6a86cff250e
NataliaEspinola <43451753+NataliaEspinola@users.noreply.github.com>
2020-10-25 18:41:12 -0300

    characters base

---
+++ TimeCrunchTower Unity/Assets/Scripts/CharactersBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersBase : MonoBehaviour
+{    
+    protected string name;
+    protected string description;
+    protected string cls = "Generic";
+    protected float atk;
+    protected float defense;
+    protected float health;
+    protected float mana;
+    protected string skill = "Generic";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
